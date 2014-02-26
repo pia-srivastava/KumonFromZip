8e603a05b406ce54eeca75d7214816cf3fe5c4b3
Pia Srivastava <pia_negandhi@yahoo.co>
2014-02-25 16:33:29 -0800

    adding skeleton LevelClearedVC

---
+++ KumonApp/KumonApp/LevelClearedViewController.h
@@ -0,0 +1,13 @@
+//
+//  LevelClearedViewController.h
+//  KumonApp
+//
+//  Created by Pia Srivastava on 2/24/14.
+//  Copyright (c) 2014 malena mesarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LevelClearedViewController : UIViewController
+
+@end
