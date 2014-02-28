cc1a991c75b209414fe457a12c4fa6d37a124b11
Pia Srivastava <pia_negandhi@yahoo.com>
2014-02-27 16:25:49 -0800

    updates

--- KumonApp/LoginViewController.m
+++ KumonApp/LoginViewController.m
@@ -10,6 +10,7 @@
 #import "AssignmentsTableViewController.h"
 #import "B49ViewController.h"
 #import <Parse/Parse.h>
+#import "LevelClearedViewController.h"
 
 @interface LoginViewController ()
 @property (nonatomic, strong) UITextField *NameText;
@@ -18,6 +19,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *passwordArea;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 - (IBAction)onB49Button:(id)sender;
+- (IBAction)onLevelClearedButton:(id)sender;
 
 @end
 
@@ -64,17 +66,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
- - (IBAction)onLoginButton:(id)sender {
- 
- NSLog(@"onLoginButton") ;
- PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
- testObject[@"foo"] = @"bar";
- [testObject saveInBackground];
- //Go to Parse
- }
- */
-
 
 #pragma - TextField Delegates
 
@@ -91,42 +82,6 @@
 // This happens when user changes editing focus to another control.
 // You can use this method to prompt user that text entered was invalid via an overlay view
 // and ask to correct text
-
-
-
-//- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
-//
-//	if (textField.tag == 1) {
-//		self.NameText = (UITextField*)[self.view viewWithTag:1];
-//
-//	}
-//
-//	// Check if userName and password is in the Parser DB
-//	if (textField.tag == 2) { // if password was entered
-//		self.PasswordText = (UITextField*) [self.view viewWithTag:2];
-//		PFQuery *query = [PFQuery queryWithClassName:@"Student"];
-//		[query whereKey:@"Username" equalTo:self.NameText.text];
-//		[query whereKey:@"Password" equalTo:self.PasswordText.text];
-//		[query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-//			if (!error) {
-//				//Success in finding user
-//				NSLog(@"User was found. OBject count = %d", objects.count);
-//			} else {
-//				// Ned to think abou
-//				NSLog(@"Username is not in DB");
-//
-//			}
-//		}];
-//	}
-//
-//
-//	return YES;
-//}
-
-// Method is called when Textfield has been asked to reisgn first responder status.
-// This happens when user changes editing focus to another control.
-// You can use this method to prompt user that text entered was invalid via an overlay view
-// and ask to correct text
 - (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
 	
 	if (textField.tag == 1) {
@@ -137,48 +92,6 @@
         self.PasswordText = (UITextField*) [self.view viewWithTag:2];
     }
     
-    //For debugging--gets back all the data within a class name
-    //    PFQuery *query = [PFQuery queryWithClassName:@"Student"];
-    //    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-    //        if (!error) {
-    //            NSLog(@"Objects: [%@]", objects);
-    //            for (NSString *object in objects) {
-    //                //[array valueForKey:specifyingKey]
-    //                NSLog(@"username is [%@]",[objects valueForKey:@"UserName"]);
-    //                NSLog(@"password is [%@]",[objects valueForKey:@"Password"]);
-    //            }
-    //            // The find succeeded. The first 100 objects are available in objects
-    //        } else {
-    //            // Log details of the failure
-    //            NSLog(@"Error: %@ %@", error, [error userInfo]);
-    //        }
-    //    }];
-    
-    
-    //	NSLog(@"self.NameText.text is %@", self.NameText.text);
-    //   	NSLog(@"self.PasswordText.text is %@", self.PasswordText.text);
-    //
-    //	// Check if userName and password is in the Parser DB
-    //	if (textField.tag == 2) { // if password was entered
-    //		self.PasswordText = (UITextField*) [self.view viewWithTag:2];
-    //		PFQuery *query = [PFQuery queryWithClassName:@"Student"];
-    //		[query whereKey:@"UserName" equalTo:self.NameText.text];
-    //		[query whereKey:@"Password" equalTo:self.PasswordText.text];
-    //		[query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-    //			if (!error && objects && objects.count == 1) {
-    //				//Success in finding user
-    //                NSLog(@"ObjectsAgain: [%@]", objects);
-    //				NSLog(@"User was found. OBject count = %d", objects.count);
-    //			} else {
-    //				// Ned to think about what to do if invalid username/password
-    //				NSLog(@"Username is not in DB");
-    //
-    //
-    //			}
-    //		}];
-    //	}
-    //
-	
 	return YES;
 }
 
@@ -188,7 +101,6 @@
 // Method is called when Textfield has resigned first responder status.
 // You would use this methods if you need your delegate to hide overaly views
 // that should only appeared while editing text.
-
 - (void)textFieldDidEndEditing:(UITextField *)textField {
 	[textField resignFirstResponder];
 	return;
@@ -197,7 +109,6 @@
 // Method is called when the the built-in clear button is pressed inside TextField
 // This button is enabled via property "clearButtonMode"
 // This method is also called when editing begins and the clearsOnBeginEditing property of the text field is set to YES.
-
 - (BOOL)textFieldShouldClear:(UITextField *)textField {
 	
 	return YES;
@@ -318,18 +229,14 @@
     
 }
 
-//- (IBAction)loginButtonAction:(UIButton *)sender {
-//
-//	NSLog(@"In loginButtonAction");
-//	AssignmentsTableViewController *assignmentsTableViewController = [[AssignmentsTableViewController alloc]initWithNibName:@"AssignmentsTableViewController" bundle:nil];
-//
-//	[self.navigationController pushViewController:assignmentsTableViewController animated:YES];
-//
-//
-//
-//}
 - (IBAction)onB49Button:(id)sender {
     B49ViewController *b49VC = [[B49ViewController alloc] init];
     [self presentViewController:b49VC animated:YES completion:NULL];
+}
+
+- (IBAction)onLevelClearedButton:(id)sender {
+    LevelClearedViewController *levelClearedVC = [[LevelClearedViewController alloc] init];
+    [self presentViewController:levelClearedVC animated:YES completion:NULL];
+    
 }
 @end
