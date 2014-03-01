9b819946b1562bba54d29028fe0a7ceb13ae8297
Pia Srivastava <pia_negandhi@yahoo.com>
2014-03-01 13:23:44 -0800

    adding images for b47

---
+++ KumonApp/KumonApp/B47ViewController.h
@@ -0,0 +1,13 @@
+//
+//  B47ViewController.h
+//  KumonApp
+//
+//  Created by Pia Srivastava on 2/28/14.
+//  Copyright (c) 2014 malena mesarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface B47ViewController : UIViewController
+
+@end
---
+++ KumonApp/KumonApp/B47ViewController.m
@@ -0,0 +1,38 @@
+//
+//  B47ViewController.m
+//  KumonApp
+//
+//  Created by Pia Srivastava on 2/28/14.
+//  Copyright (c) 2014 malena mesarina. All rights reserved.
+//
+
+#import "B47ViewController.h"
+
+@interface B47ViewController ()
+
+@end
+
+@implementation B47ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
